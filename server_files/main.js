e52ff51be62633202ff9dc3cfe4d35c704b5a7e9
theRaznet <borjamacuer@gmail.com>
2018-02-12 15:40:26 -0300

    git.commit

--- server_files/main.js
+++ server_files/main.js
@@ -5,11 +5,13 @@ function preload(){
     game.load.image('background', 'assets/Room-1.png');
     game.load.image('ground', 'assets/platform.jpg');
     game.load.image('star', 'assets/star.png');
+    game.load.image('ember', 'assets/ember.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48, 9);
 }
 var player;
 
 var border;
+var platform;
 
 var stars;
 
@@ -26,25 +28,47 @@ function create(){
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.add.sprite(0, 0, 'background').scale.setTo(scaleFactor, scaleFactor);
+    game.add.sprite('ember').scale.setTo(scaleFactor, scaleFactor);
 
     border = game.add.group();
     border.enableBody = true;
-    
-    var bottom = border.create(game.world.height - 800, 510, null);
+
+    var bottom = border.create(game.world.height - 800, 510, 'ground');
     bottom.scale.setTo(39, 1);
     bottom.body.immovable = true;
 
-    var top = border.create(0, 51, null);
+    var top = border.create(0, 51, 'ground');
     top.scale.setTo(38, 1);
     top.body.immovable = true;
 
-    var left = border.create(90, 0, null);
+    var left = border.create(90, 0, 'ground');
     left.scale.setTo(1, 20);
     left.body.immovable = true;
 
-    var right = border.create(game.world.width - 250, 0, null);
+    var right = border.create(game.world.width - 250, 0, 'ground');
     right.scale.setTo(1, 20);
     right.body.immovable = true;
+
+
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    
+    var ledge = platforms.create(275, 200, 'ember');
+    ledge.scale.setTo(.75, .75);
+    ledge.body.immovable = true;
+
+    var ledge = platforms.create(275, 400, 'ember');
+    ledge.scale.setTo(.75, .75);
+    ledge.body.immovable = true;
+
+    var ledge = platforms.create(850, 200, 'ember');
+    ledge.scale.setTo(.75, .75);
+    ledge.body.immovable = true;
+
+    var ledge = platforms.create(850, 400, 'ember');
+    ledge.scale.setTo(.75, .75);
+    ledge.body.immovable = true;
 
     //player = game.add.sprite(256, 250, 'dude');
     player = new Player(game, 256, 250);
@@ -56,7 +80,7 @@ function create(){
     for (var i = 0; i < 30; i++){
         for (var j = 0; j < 8; j++){
             if (getRandomInt(0, 2) == 1){
-                var star = stars.create(i * 32 + 192 + 4, j * 48 + 256 + 8, 'star');
+                var star = stars.create(i * 32 + 128 + 4, j * 48 + 128 + 8, 'star');
             }
         }
     }
@@ -68,8 +92,8 @@ function create(){
 }
 
 function update(){
-    //score++;
     game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    //game.physics.arcade.overlap(ledge, stars, collectStar, null, this);
     game.physics.arcade.collide(player, border, collisionHandler, null, this);
 
     if (cursors.up.isDown) {
